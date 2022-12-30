936c028498443be5f06b9322645585991d581df8
Daniel Armando Castiblanco Fajardo <44913158+Twynzen@users.noreply.github.com>
2022-12-29 22:02:31 -0500

    feat: create const with questions

--- src/app/butlerForm/butler-form/butler-form.component.ts
+++ src/app/butlerForm/butler-form/butler-form.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Question } from 'src/app/Interfaces/questions.interface';
+import { QuestionsFamiliarity } from './questions.const';
 
 @Component({
   selector: 'app-butler-form',
@@ -10,43 +10,8 @@ import { Question } from 'src/app/Interfaces/questions.interface';
 export class ButlerFormComponent {
   formPersonData: FormGroup;
   currentQuestion = 0;
+  questions = QuestionsFamiliarity;
 
- questions: Question[] = [
-    {
-      label: '¿Cómo deseas llamarme?',
-      name: 'nameIa',
-      validator: Validators.required,
-      type: 'text',
-    },
-    {
-      label: '¿Quién eres? Dime como te gustaría que te llamara',
-      name: 'nameUser',
-      validator: Validators.required,
-      type: 'text',
-    },
-    {
-      label: '¿Cuál es tu edad?',
-      name: 'age',
-      validator: Validators.required,
-      type: 'text',
-    },
-    {
-      label: '¿Cuál es tu ocupación o profesión?',
-      name: 'occupation',
-      validator: Validators.required,
-      type: 'text',
-    },
-   {
-     label: '¿Te gusta más hacer actividades al aire libre o en interiores? ¿Qué tipo de actividades te gusta realizar en tu tiempo libre?',
-     name: 'freeTimeActivities',
-     validator: Validators.required,
-     type: 'radio',
-     options: [
-       { label: 'Al aire libre', value: 'outdoor' },
-       { label: 'En interiores', value: 'indoor' }
-     ]
-   },
-  ];
 
   constructor(private formBuilder: FormBuilder) {
     this.formPersonData = this.formBuilder.group({
